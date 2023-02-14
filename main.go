81f0d3d89bb458b3e8dac7567c77debed403503d
Anil K Rajput <anil.k.rajput@encodingenhancers.com>
2023-02-14 16:53:36 +0530

    Using other modules in main module

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"main/telegraf"
 )
 
 func main() {
 	Method1()
+	telegraf.MethodTelegraf()
 }
 func Method1() {
 	fmt.Println("This is the method from the main file")
